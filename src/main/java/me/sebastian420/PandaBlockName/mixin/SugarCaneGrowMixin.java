a2eb610a2b81a49680f63211307b1d3e81bbec42
Sebastian <sebastian97@gmail.com>
2024-07-11 21:26:06 -0700

    Update SugarCaneGrowMixin.java

--- src/main/java/me/sebastian420/PandaBlockName/mixin/SugarCaneGrowMixin.java
+++ src/main/java/me/sebastian420/PandaBlockName/mixin/SugarCaneGrowMixin.java
@@ -1,4 +1,25 @@
 package me.sebastian420.PandaBlockName.mixin;
 
+import me.sebastian420.PandaBlockName.EmptyBlockEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SugarCaneBlock.class)
 public class SugarCaneGrowMixin {
+    @Inject(method = "randomTick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z", ordinal = 0))
+    protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof EmptyBlockEntity) {
+            world.addBlockEntity(new EmptyBlockEntity(pos.up(),world.getBlockState(pos.up())));
+            world.getBlockEntity(pos.up()).setComponents(blockEntity.getComponents());
+        }
+    }
 }
