aa1d4af09d71fe8f4b99a8ea7657b29bc0c31995
Sebastian <sebastian97@gmail.com>
2024-07-11 21:55:33 -0700

    Small mushroom

---
+++ src/main/java/me/sebastian420/PandaBlockName/mixin/MushroomGrowMixin.java
@@ -0,0 +1,28 @@
+package me.sebastian420.PandaBlockName.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import me.sebastian420.PandaBlockName.EmptyBlockEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.MushroomPlantBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MushroomPlantBlock.class)
+public class MushroomGrowMixin {
+    @Inject(method = "randomTick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+    protected void grow(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci,
+                        @Local(ordinal = 1) BlockPos blockPos) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof EmptyBlockEntity) {
+            world.addBlockEntity(new EmptyBlockEntity(blockPos,world.getBlockState(blockPos)));
+            world.getBlockEntity(blockPos).setComponents(blockEntity.getComponents());
+        }
+    }
+}
