eeb12fa4d554fa1f51075badaaed3d0643713c81
Sebastian <sebastian97@gmail.com>
2024-07-12 01:43:58 -0700

    Bamboo fixed

---
+++ src/main/java/me/sebastian420/PandaBlockName/mixin/BambooOtherGrowMixin.java
@@ -0,0 +1,25 @@
+package me.sebastian420.PandaBlockName.mixin;
+
+import me.sebastian420.PandaBlockName.EmptyBlockEntity;
+import net.minecraft.block.BambooBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BambooBlock.class)
+public class BambooOtherGrowMixin {
+    @Inject(method = "updateLeaves", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", ordinal = 2 , shift = At.Shift.AFTER))
+    protected void grow(BlockState state, World world, BlockPos pos, Random random, int height, CallbackInfo ci) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof EmptyBlockEntity) {
+            world.addBlockEntity(new EmptyBlockEntity(pos.up(),world.getBlockState(pos.up())));
+            world.getBlockEntity(pos.up()).setComponents(blockEntity.getComponents());
+        }
+    }
+}
