165b8226e85bf59f92fd79358ba01785dce74096
Sebastian <sebastian97@gmail.com>
2024-07-11 21:39:38 -0700

    ChorusFruit

--- src/main/java/me/sebastian420/PandaBlockName/mixin/ChorusFlowerGrowMixin.java
+++ src/main/java/me/sebastian420/PandaBlockName/mixin/ChorusFlowerGrowMixin.java
@@ -1,4 +1,37 @@
 package me.sebastian420.PandaBlockName.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import me.sebastian420.PandaBlockName.EmptyBlockEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChorusFlowerBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChorusFlowerBlock.class)
 public class ChorusFlowerGrowMixin {
+    @Inject(method = "randomTick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/ChorusFlowerBlock;grow(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;I)V", ordinal = 0))
+    private void grow(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci,
+                      @Local(ordinal = 1) BlockPos blockPos) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof EmptyBlockEntity) {
+            world.addBlockEntity(new EmptyBlockEntity(blockPos,world.getBlockState(blockPos)));
+            world.getBlockEntity(blockPos).setComponents(blockEntity.getComponents());
+        }
+    }
+
+    @Inject(method = "randomTick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/ChorusFlowerBlock;grow(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;I)V", ordinal = 1))
+    private void growTwo(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci,
+                      @Local(ordinal = 2) BlockPos blockPos) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof EmptyBlockEntity) {
+            world.addBlockEntity(new EmptyBlockEntity(blockPos,world.getBlockState(blockPos)));
+            world.getBlockEntity(blockPos).setComponents(blockEntity.getComponents());
+        }
+    }
 }
