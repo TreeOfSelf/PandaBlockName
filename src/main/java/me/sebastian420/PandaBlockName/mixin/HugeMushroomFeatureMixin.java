e119a339ff2f7a0446b268c2b88ae591fc125de2
Sebastian <sebastian97@gmail.com>
2025-05-04 23:27:34 -0700

    Update HugeMushroomFeatureMixin.java

--- src/main/java/me/sebastian420/PandaBlockName/mixin/HugeMushroomFeatureMixin.java
+++ src/main/java/me/sebastian420/PandaBlockName/mixin/HugeMushroomFeatureMixin.java
@@ -1,5 +1,6 @@
 package me.sebastian420.PandaBlockName.mixin;
 
+import me.sebastian420.PandaBlockName.BlockEntityPlacer;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
@@ -14,25 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HugeMushroomFeature.class)
 public class HugeMushroomFeatureMixin {
-    @Unique
-    private static ThreadLocal<BlockPos> ORIGINAL_POS = new ThreadLocal<>();
+    @Inject(method = "generateStem(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/gen/feature/HugeMushroomFeatureConfig;ILnet/minecraft/util/math/BlockPos$Mutable;)V",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/feature/HugeMushroomFeature;generateStem(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos$Mutable;Lnet/minecraft/block/BlockState;)V",
+    ordinal = 0, shift = At.Shift.AFTER))
 
-    @Inject(
-            method = "generateStem(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/gen/feature/HugeMushroomFeatureConfig;ILnet/minecraft/util/math/BlockPos$Mutable;)V",
-            at = @At("HEAD")
-    )
-    private void storeOriginalPos(WorldAccess world, Random random, BlockPos pos,
-                                  HugeMushroomFeatureConfig config, int height,
-                                  BlockPos.Mutable mutablePos, CallbackInfo ci) {
-        ORIGINAL_POS.set(pos.toImmutable());
+    private void onGenerateStem(WorldAccess world, Random random, BlockPos pos,
+                                HugeMushroomFeatureConfig config, int height,
+                                BlockPos.Mutable mutablePos, CallbackInfo ci) {
+        BlockEntityPlacer.move(world, pos, mutablePos);
     }
 
-    @Inject(
-            method = "generateStem(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos$Mutable;Lnet/minecraft/block/BlockState;)V",
-            at = @At("TAIL")
-    )
-    private void onSetBlockState(WorldAccess world, BlockPos.Mutable pos, BlockState state, CallbackInfo ci) {
-        BlockPos originalPos = ORIGINAL_POS.get();
-
-    }
 }
