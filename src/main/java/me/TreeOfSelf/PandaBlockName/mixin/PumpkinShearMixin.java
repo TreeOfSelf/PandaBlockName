3d61e013b61c54153b8e8ac8c7cb5847154a418a
Sebastian <sebastian97@gmail.com>
2025-07-10 01:42:57 -0700

    Fix pumpkin recursion crash

--- src/main/java/me/TreeOfSelf/PandaBlockName/mixin/PumpkinShearMixin.java
+++ src/main/java/me/TreeOfSelf/PandaBlockName/mixin/PumpkinShearMixin.java
@@ -31,63 +31,62 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(PumpkinBlock.class)
 public abstract class PumpkinShearMixin {
-    @Shadow protected abstract ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit);
 
     @Inject(method = "onUseWithItem", at = @At(value = "HEAD"), cancellable = true)
-    private ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+    private void onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         if (!PandaBlockNameConfig.isVegetationFeatureEnabled("PumpkinShearing")) {
-            return onUseWithItem(stack, state, world, pos, player, hand, hit);
+            return;
         }
-        
+
         if (!stack.isOf(Items.SHEARS)) {
-            return onUseWithItem(stack, state, world, pos, player, hand, hit);
-        } else if (world.isClient) {
-            return ActionResult.SUCCESS;
-        } else {
-            Direction direction = hit.getSide();
-            Direction direction2 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
-            world.playSound((Entity)null, pos, SoundEvents.BLOCK_PUMPKIN_CARVE, SoundCategory.BLOCKS, 1.0F, 1.0F);
-            world.setBlockState(pos, (BlockState) Blocks.CARVED_PUMPKIN.getDefaultState().with(CarvedPumpkinBlock.FACING, direction2), 11);
-
-            ItemStack modifiedSeeds = new ItemStack(Items.PUMPKIN_SEEDS, 4);
-            BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity != null) {
-                if (blockEntity.getComponents().contains(DataComponentTypes.CUSTOM_NAME)) {
-
-                    Text customName = blockEntity.getComponents().get(DataComponentTypes.CUSTOM_NAME);
-                    if (customName.getString().startsWith("{")) {
-                        try {
-                            JsonElement jsonElement = JsonParser.parseString(blockEntity.getComponents().get(DataComponentTypes.CUSTOM_NAME).getString());
-                            DataResult<Pair<Text, JsonElement>> result = TextCodecs.CODEC.decode(JsonOps.INSTANCE, jsonElement);
-                            customName = result.getOrThrow().getFirst();
-                        } catch (Exception ignored) {
-                        }
-                    }
-
-                    modifiedSeeds.set(DataComponentTypes.CUSTOM_NAME, customName);
-                }
-                if (blockEntity.getComponents().contains(DataComponentTypes.LORE)) {
-                    modifiedSeeds.set(DataComponentTypes.LORE, blockEntity.getComponents().get(DataComponentTypes.LORE));
-                }
-            }
-
-            ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + 0.5 + (double)direction2.getOffsetX() * 0.65, (double)pos.getY() + 0.1, (double)pos.getZ() + 0.5 + (double)direction2.getOffsetZ() * 0.65, modifiedSeeds);
-            itemEntity.setVelocity(0.05 * (double)direction2.getOffsetX() + world.random.nextDouble() * 0.02, 0.05, 0.05 * (double)direction2.getOffsetZ() + world.random.nextDouble() * 0.02);
-            world.spawnEntity(itemEntity);
-            stack.damage(1, player, LivingEntity.getSlotForHand(hand));
-            world.emitGameEvent(player, GameEvent.SHEAR, pos);
-            player.incrementStat(Stats.USED.getOrCreateStat(Items.SHEARS));
-            cir.setReturnValue(ActionResult.SUCCESS);
-            return ActionResult.SUCCESS;
+            return;
         }
+
+        if (world.isClient) {
+            cir.setReturnValue(ActionResult.SUCCESS);
+            return;
+        }
+
+        Direction direction = hit.getSide();
+        Direction direction2 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
+        world.playSound(null, pos, SoundEvents.BLOCK_PUMPKIN_CARVE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        world.setBlockState(pos, Blocks.CARVED_PUMPKIN.getDefaultState().with(CarvedPumpkinBlock.FACING, direction2), 11);
+
+        ItemStack modifiedSeeds = new ItemStack(Items.PUMPKIN_SEEDS, 4);
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity != null) {
+            if (blockEntity.getComponents().contains(DataComponentTypes.CUSTOM_NAME)) {
+
+                Text customName = blockEntity.getComponents().get(DataComponentTypes.CUSTOM_NAME);
+                if (customName.getString().startsWith("{")) {
+                    try {
+                        JsonElement jsonElement = JsonParser.parseString(blockEntity.getComponents().get(DataComponentTypes.CUSTOM_NAME).getString());
+                        DataResult<Pair<Text, JsonElement>> result = TextCodecs.CODEC.decode(JsonOps.INSTANCE, jsonElement);
+                        customName = result.getOrThrow().getFirst();
+                    } catch (Exception ignored) {
+                    }
+                }
+
+                modifiedSeeds.set(DataComponentTypes.CUSTOM_NAME, customName);
+            }
+            if (blockEntity.getComponents().contains(DataComponentTypes.LORE)) {
+                modifiedSeeds.set(DataComponentTypes.LORE, blockEntity.getComponents().get(DataComponentTypes.LORE));
+            }
+        }
+
+        ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + 0.5 + (double)direction2.getOffsetX() * 0.65, (double)pos.getY() + 0.1, (double)pos.getZ() + 0.5 + (double)direction2.getOffsetZ() * 0.65, modifiedSeeds);
+        itemEntity.setVelocity(0.05 * (double)direction2.getOffsetX() + world.random.nextDouble() * 0.02, 0.05, 0.05 * (double)direction2.getOffsetZ() + world.random.nextDouble() * 0.02);
+        world.spawnEntity(itemEntity);
+        stack.damage(1, player, LivingEntity.getSlotForHand(hand));
+        world.emitGameEvent(player, GameEvent.SHEAR, pos);
+        player.incrementStat(Stats.USED.getOrCreateStat(Items.SHEARS));
+        cir.setReturnValue(ActionResult.SUCCESS);
     }
 }
